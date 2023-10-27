1e3119e2f659880af2ff8ee40469a58bb0c77ba1
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 13:32:40 +0200

    End

--- main.go
+++ main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"sort"
 )
 
 func main() {
-	greeting := "Hello there firends!"
+	// greeting := "hello there firends!"
 
-	fmt.Println(strings.Contains(greeting, "hello"))
+	// fmt.Println(strings.Contains(greeting, "hello"))
+	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	// fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.Index(greeting, "ll"))
+	// fmt.Println(strings.Split(greeting, " "))
+
+	// the original value is unchanged
+	// fmt.Println("original string was: ", greeting)
+
+	//Sort
+
+	// ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+	// sort.Ints(ages)
+	// fmt.Println(ages)
+
+	// index := sort.SearchInts(ages, 30)
+	// fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+
+	sort.Strings(names)
+	fmt.Println(names)
 
 }
