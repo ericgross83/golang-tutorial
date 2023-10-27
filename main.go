ed382dc6b7cf90fddecc9dd4e72e06653084b490
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 21:09:34 +0200

    Start

--- main.go
+++ main.go
@@ -2,34 +2,16 @@ package main
 
 import "fmt"
 
+func updateName(name string) {
+	name = "wedge"
+}
+
 func main() {
 
-	menu := map[string]float64{
-		"soup":           4.99,
-		"pie":            7.99,
-		"salad":          6.99,
-		"toffee pudding": 3.55,
-	}
+	// group A types -> strings, ints, bools, floats, arrays, structs
+	name := "tifa"
 
-	fmt.Println(menu)
-	fmt.Println(menu["pie"])
+	updateName(name)
 
-	// looping maps
-	for key, item := range menu {
-		fmt.Println(key, "-", item)
-	}
-
-	// ints as key type
-	phonebook := map[int]string{
-		267584967: "mario",
-		267584154: "luigi",
-		267584758: "peach",
-	}
-
-	fmt.Println(phonebook)
-	fmt.Println(phonebook[267584967])
-
-	phonebook[267584967] = "bowser"
-
-	fmt.Println(phonebook[267584967])
+	fmt.Println(name)
 }
