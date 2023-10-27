78f55bdd8c5228590e05fc9aeb1ee32cf4788d7e
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 13:33:49 +0200

    Start

--- main.go
+++ main.go
@@ -6,29 +6,6 @@ import (
 )
 
 func main() {
-	// greeting := "hello there firends!"
-
-	// fmt.Println(strings.Contains(greeting, "hello"))
-	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	// fmt.Println(strings.ToUpper(greeting))
-	// fmt.Println(strings.Index(greeting, "ll"))
-	// fmt.Println(strings.Split(greeting, " "))
-
-	// the original value is unchanged
-	// fmt.Println("original string was: ", greeting)
-
-	//Sort
-
-	// ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
-	// sort.Ints(ages)
-	// fmt.Println(ages)
-
-	// index := sort.SearchInts(ages, 30)
-	// fmt.Println(index)
-
-	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
-
-	sort.Strings(names)
-	fmt.Println(names)
+	
 
 }
