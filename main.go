70df1ecc884f77f5b5c9c3bd4321ba632d42638b
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 13:43:37 +0200

    End

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
 func main() {
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x+1)
+	// 	x++
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("value of i is:", i+1)
+	// }
+
+	names := []string{"yoshi", "mario", "luigi", "peach"}
+
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
+
+	// for index, name := range names {
+	// 	fmt.Printf("The value at index %v is %v\n", index, name)
+	// }
+
+	for _, name := range names {
+		fmt.Printf("The value is %v \n", name)
+	}
 
 }
