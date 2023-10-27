61583ac86a06fa5ce626987ac23869633a96fc30
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 15:10:23 +0200

    End

--- main.go
+++ main.go
@@ -1,4 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
+	}
+
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for key, item := range menu {
+		fmt.Println(key, "-", item)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		267584967: "mario",
+		267584154: "luigi",
+		267584758: "peach",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[267584967])
+
+	phonebook[267584967] = "bowser"
+
+	fmt.Println(phonebook[267584967])
 }
