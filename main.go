e0f8bca6f6ef34ba7a2032fbadb2cff69e52e6f4
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 14:24:34 +0200

    End

--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sayGreeting(n string) {
+	fmt.Printf("Good morning %v \n", n)
+}
+
+func sayBye(n string) {
+	fmt.Printf("Goodbye %v \n", n)
+}
+
+func cycleNames(n []string, f func(string)) {
+	for _, v := range n {
+		f(v)
+	}
+}
+
+func circleArea(r float64) float64 {
+	return math.Pi * r * r
+}
 
 func main() {
-	age := 45
+	// len() append()
+	// sayGreeting("Eric")
+	// sayBye("Eric")
+	// cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
+	// cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
 
-	fmt.Println(age <= 50)
-	fmt.Println(age >= 50)
-	fmt.Println(age == 45)
-	fmt.Println(age != 50)
+	a1 := circleArea(10.5)
+	a2 := circleArea(15)
 
-	if age < 30 {
-		fmt.Println("age is less than 30")
-	} else if age < 40 {
-		fmt.Println("age is less than 40")
-	} else {
-		fmt.Println("age is NOT less then 40")
-	}
+	fmt.Println(a1, a2)
+	fmt.Printf("Circle 1 is %0.3f and cicle 2 is %0.3f", a1, a2)
 }
