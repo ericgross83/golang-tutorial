6efebee86f1e8eb269e851b565eb0534c5d7a0c8
Eric Gross <eric.gross@volkswagen.de>
2023-10-27 14:49:50 +0200

    Start

--- main.go
+++ main.go
@@ -1,33 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-func getInitials(n string) (string, string) {
-	upper := strings.ToUpper(n)
-	names := strings.Split(upper, " ")
-
-	var initials []string
-	for _, value := range names {
-		initials = append(initials, value[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
-
 func main() {
-	initial1, initial2 := getInitials("tifa lockhart")
-	fmt.Println(initial1, initial2)
-
-	initial3, initial4 := getInitials("cloud strife")
-	fmt.Println(initial3, initial4)
-
-	initial5, initial6 := getInitials("barret")
-	fmt.Println(initial5, initial6)
 }
